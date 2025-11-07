e94e74e81e1cde7b38b390b207fc75fc6c4acdbf
JshuaMguel <jshua.miguel@example.com>
2025-11-07 20:41:46 +0800

    Fix: Remove global session middleware causing 500 error on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\RoleMiddleware;
-use App\Http\Middleware\EnsureSessionWorks;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,9 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
-        
-        // Add session middleware globally for production compatibility
-        $middleware->append(EnsureSessionWorks::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
